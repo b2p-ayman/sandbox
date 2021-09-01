67ca328dc495d9b699de328c566015614208badf
Ayman <abouassab@b2pweb.com>
2021-09-01 12:32:30 +0200

    Update tests

--- tests/Func/Entity/UserTest.php
+++ tests/Func/Entity/UserTest.php
@@ -20,40 +20,12 @@ class UserTest extends AbstractEndPoint
         self::assertNotEmpty($responseDecoded);
     }
 
-    public function testGetFiles(): void
-    {
-        $response = $this->getResponseFromRequest(Request::METHOD_GET, '/api/files');
-        $responseContent = $response->getContent();
-        $responseDecoded = json_decode($responseContent);
-
-        self::assertResponseIsSuccessful();
-        self::assertJson($responseContent);
-        self::assertNotEmpty($responseDecoded);
-    }
-
     public function testPostUser(): void
     {
         $response = $this->getResponseFromRequest(
             Request::METHOD_POST,
             '/api/users',
             $this->getPayload()
-        );
-        $responseContent = $response->getContent();
-        $responseDecoded = json_decode($responseContent);
-
-        self::assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
-        self::assertJson($responseContent);
-        self::assertNotEmpty($responseDecoded);
-    }
-
-    public function testPostFile(): void
-    {
-        $filePayload = '{"titre" : "test titre", "description":"test description","user":"/api/users/3"}';
-
-        $response = $this->getResponseFromRequest(
-            Request::METHOD_POST,
-            '/api/files',
-            $filePayload
         );
         $responseContent = $response->getContent();
         $responseDecoded = json_decode($responseContent);
